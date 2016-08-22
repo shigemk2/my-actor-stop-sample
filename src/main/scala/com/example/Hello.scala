6daf5f03d507c0f88e02bd8ca3580a3109e5b9e6
shigemk2 <i.am.shige@gmail.com>
2016-08-23 02:16:33 +0900

    Fix Hello

--- src/main/scala/com/example/Hello.scala
+++ src/main/scala/com/example/Hello.scala
@@ -1,7 +1,30 @@
 package com.example
 
+import akka.actor.{ActorSystem, Props}
+import akka.util.Timeout
+
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import akka.pattern.ask
+
 object Hello {
   def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    val system = ActorSystem("mySystem")
+
+    val props = Props[MyActor]
+    val actor = system.actorOf(props, name = "myActor")
+    Thread.sleep(1000)
+
+    implicit val timeout = Timeout(5 seconds)
+    val future: Future[Any] = actor ? "kill"
+
+    val result = Await.result(future, timeout.duration).asInstanceOf[String]
+
+    println(result)
+
+    system.stop(actor)
+    Thread.sleep(1000)
+
+    system.terminate()
   }
 }
